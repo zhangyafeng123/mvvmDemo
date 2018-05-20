8d481899220248c1d34bae41d92846580db1e200
zhangyafeng123 <1240334911@qq.com>
2018-05-20 12:52:55 +0800

    Initial Commit

---
+++ MVVM动态Cell/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MVVM动态Cell
+//
+//  Created by linjianguo on 2018/5/20.
+//  Copyright © 2018年 com.justsee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MVVM动态Cell/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MVVM动态Cell
+//
+//  Created by linjianguo on 2018/5/20.
+//  Copyright © 2018年 com.justsee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
